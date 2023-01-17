571eddd6e3d93e34cd7c7fc32575ae9ab84acf68
David O Neill <dmz.oneill@gmail.com>
2023-01-17 13:27:59 +0000

    fix check

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ for X in $(curl "https://api.github.com/users/$user/repos?per_page=100" | jq -r 
   action_file="https://github.com/$user/$name/blob/master/.github/workflows/main.yml"
   exists=$(curl -s -o /dev/null -w "%{http_code}" "$action_file")
   
-  [[ "$exists" == "200" ]] && echo "Skip action exists" && continue
+  [[ "$exists" != "404" ]] && echo "Skip action exists" && continue
 
   git_url=https://$user:$pass@github.com/$user/$name.git
   git clone "$git_url"

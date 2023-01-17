2b471293af6061ac522666605099d12141d094eb
David O Neill <dmz.oneill@gmail.com>
2023-01-17 18:40:05 +0000

    follow redirect

--- setup.sh
+++ setup.sh
@@ -4,35 +4,46 @@ user=dmzoneill
 email=dmz.oneill@gmail.com
 pass=$PROFILE_HOOK
 
-for X in $(curl "https://api.github.com/users/$user/repos?per_page=100" | jq -r '.[] | .ssh_url'); do
-  name=$(echo "$X" | awk -F'/' '{print $2}' | sed 's/\.git//')
-  echo "$name"
+page=1
+while true; do
+  processed=0
+  for X in $(curl "https://api.github.com/users/$user/repos?per_page=100&page=$page" | jq -r '.[] | .ssh_url'); do
+    name=$(echo "$X" | awk -F'/' '{print $2}' | sed 's/\.git//')
+    echo "$name"
 
-  [[ "$name" == "dmzoneill" ]] && continue
+    [[ "$name" == "dmzoneill" ]] && continue
 
-  action_file="https://github.com/$user/$name/blob/master/.github/workflows/main.yml"
-  exists=$(curl -L -s -o /dev/null -w "%{http_code}" "$action_file")
-  
-  [[ "$exists" != "404" ]] && echo "Skip action exists" && continue
+    action_file="https://github.com/$user/$name/blob/master/.github/workflows/main.yml"
+    exists=$(curl -L -s -o /dev/null -w "%{http_code}" "$action_file")
+    
+    [[ "$exists" != "404" ]] && echo "Skip action exists" && continue
 
-  git_url=https://$user:$pass@github.com/$user/$name.git
-  git clone "$git_url"
+    git_url=https://$user:$pass@github.com/$user/$name.git
+    git clone "$git_url"
 
-  [ ! -f "$name/LICENSE" ] && cp LICENSE "$name/"
-  
-  mkdir -vp "$name/.github/workflows/"
-  cp main.yml "$name/.github/workflows/"
-  
-  (
-    cd "$name" || exit 1
-    gh secret set profile_hook -r "$user/$name" -b "$pass"
-    git config --global user.email $email
-    git config --global user.name $user
+    [ ! -f "$name/LICENSE" ] && cp LICENSE "$name/"
+    
+    mkdir -vp "$name/.github/workflows/"
+    cp main.yml "$name/.github/workflows/"
+    
+    (
+      cd "$name" || exit 1
+      gh secret set profile_hook -r "$user/$name" -b "$pass"
+      git config --global user.email $email
+      git config --global user.name $user
 
-    git remote set-url origin "$git_url"
-    git add -A
-    git commit -a -m "add github action and setup secret"
-    git pull --rebase
-    git push 
-  )
+      git remote set-url origin "$git_url"
+      git add -A
+      git commit -a -m "add github action and setup secret"
+      git pull --rebase
+      git push 
+    )
+    let processed++
+  done
+
+  if [ $processed -le 1 ]]; then
+    exit 0;
+  fi
+
+  let page++
 done

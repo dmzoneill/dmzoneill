1abd061cea54d68495f3e1305a6fe19efe7989eb
David O Neill <dmz.oneill@gmail.com>
2023-01-17 11:05:23 +0000

    automatic repo update

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+COUNTER=1
+for X in `curl 'https://api.github.com/users/dmzoneill/repos?per_page=100' | jq -r '.[] | .ssh_url'`; do
+  printf "%d\n" $COUNTER
+  let COUNTER=COUNTER+1
+  name=$(echo $X | awk -F'/' '{print $2}' | sed 's/\.git//')
+  exists=$(curl -s -o /dev/null -w "%{http_code}" https://github.com/dmzoneill/$name/blob/master/.github/workflows/main.yml)
+  
+  [[ "$exists" == "404" ]] && git clone $X;
+  [ ! -f "$name/LICENSE" ] && cp LICENSE "$name/"
+  
+  mkdir -vp "$name/.github/workflows/"
+  cp main.yml "$name/.github/workflows/"
+  
+  cd "$name"
+  gh secret set profile_hook -r "dmzoneill/$name" -b "{{ secrets.PROFILE_HOOK }}"
+  git add -A
+  git commit -a -m "add github action and setup secret"
+  git pull --rebase
+  git push 
+  cd ..  
+done

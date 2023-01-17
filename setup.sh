7e7930b4e445e353725af1b142561272499678b2
David O Neill <dmz.oneill@gmail.com>
2023-01-17 12:54:10 +0000

    automatic repo update

--- setup.sh
+++ setup.sh
@@ -4,31 +4,32 @@ user=dmzoneill
 email=dmz.oneill@gmail.com
 pass=$PROFILE_HOOK
 
-for X in `curl "https://api.github.com/users/$user/repos?per_page=100" | jq -r '.[] | .ssh_url'`; do
-  name=$(echo $X | awk -F'/' '{print $2}' | sed 's/\.git//')
-  echo $name
+for X in $(curl "https://api.github.com/users/$user/repos?per_page=100" | jq -r '.[] | .ssh_url'); do
+  name=$(echo "$X" | awk -F'/' '{print $2}' | sed 's/\.git//')
+  echo "$name"
   action_file="https://github.com/$user/$name/blob/master/.github/workflows/main.yml"
-  exists=$(curl -s -o /dev/null -w "%{http_code}" $action_file)
+  exists=$(curl -s -o /dev/null -w "%{http_code}" "$action_file")
   
   [[ "$exists" == "200" ]] && echo "Skip action eixsts" && continue
 
   git_url=https://$user:$pass@github.com/$user/$name.git
-  git clone $git_url
+  git clone "$git_url"
 
   [ ! -f "$name/LICENSE" ] && cp LICENSE "$name/"
   
-  mkdir -vp $name/.github/workflows/
-  cp main.yml $name/.github/workflows/
+  mkdir -vp "$name/.github/workflows/"
+  cp main.yml "$name/.github/workflows/"
   
-  cd $name
-  gh secret set profile_hook -r $user/$name -b $pass
-  git config --global user.email $email
-  git config --global user.name $user
+  (
+    cd $name || exit 1
+    gh secret set profile_hook -r "$user/$name" -b "$pass"
+    git config --global user.email $email
+    git config --global user.name $user
 
-  git remote set-url origin $git_url
-  git add -A
-  git commit -a -m "add github action and setup secret"
-  git pull --rebase
-  git push 
-  cd .. 
+    git remote set-url origin "$git_url"
+    git add -A
+    git commit -a -m "add github action and setup secret"
+    git pull --rebase
+    git push 
+  )
 done

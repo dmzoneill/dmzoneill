bebdde18232e83a8a94e623c61a01274a11a108e
David O Neill <dmz.oneill@gmail.com>
2023-01-17 12:24:59 +0000

    automatic repo update

--- setup.sh
+++ setup.sh
@@ -5,13 +5,15 @@ email=dmz.oneill@gmail.com
 
 COUNTER=1
 for X in `curl "https://api.github.com/users/$user/repos?per_page=100" | jq -r '.[] | .ssh_url'`; do
-  printf "%d\n" $COUNTER
-  let COUNTER=COUNTER+1
+  printf "%d\n" $COUNTER  
   name=$(echo $X | awk -F'/' '{print $2}' | sed 's/\.git//')
-  exists=$(curl -s -o /dev/null -w "%{http_code}" https://github.com/$user/$name/blob/master/.github/workflows/main.yml)
+  echo $name
+  repo="https://github.com/$user/$name/blob/master/.github/workflows/main.yml"
+  exists=$(curl -s -o /dev/null -w "%{http_code}" $repo)
   
   [[ "$exists" == "200" ]] && continue
-  git clone https://$user:${{ secrets.PROFILE_HOOK }}@github.com/$user/$name.git $X;
+  git_url="https://$user:${{ secrets.PROFILE_HOOK }}@github.com/$user/$name.git"
+  git clone "$git_url";
 
   [ ! -f "$name/LICENSE" ] && cp LICENSE "$name/"
   
@@ -19,16 +21,15 @@ for X in `curl "https://api.github.com/users/$user/repos?per_page=100" | jq -r '
   cp main.yml "$name/.github/workflows/"
   
   cd "$name"
-  gh secret set profile_hook -r "$user/$name" -b "{{ secrets.PROFILE_HOOK }}"
+  gh secret set profile_hook -r "$user/$name" -b "${{ secrets.PROFILE_HOOK }}"
   git config --global user.email "$email"
   git config --global user.name "$user"
 
-  url="https://$user:${{ secrets.PROFILE_HOOK }}@github.com/$user/${name}.git"
-
-  git remote set-url origin $url
+  git remote set-url origin "$git_url"
   git add -A
   git commit -a -m "add github action and setup secret"
   git pull --rebase
   git push 
-  cd ..  
+  cd .. 
+  let COUNTER=COUNTER+1 
 done

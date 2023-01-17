2ceccef10e339bee4549f4f78db1d6ef492a4b78
David O Neill <dmz.oneill@gmail.com>
2023-01-17 11:18:50 +0000

    automatic repo update

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ for X in `curl 'https://api.github.com/users/dmzoneill/repos?per_page=100' | jq 
   name=$(echo $X | awk -F'/' '{print $2}' | sed 's/\.git//')
   exists=$(curl -s -o /dev/null -w "%{http_code}" https://github.com/dmzoneill/$name/blob/master/.github/workflows/main.yml)
   
-  [[ "$exists" == "404" ]] && git clone $X;
+  [[ "$exists" == "404" ]] && git clone https://dmzoneill:${{ secrets.PROFILE_HOOK }}@github.com/dmzoneill/$name.git $X;
   [ ! -f "$name/LICENSE" ] && cp LICENSE "$name/"
   
   mkdir -vp "$name/.github/workflows/"
@@ -15,6 +15,9 @@ for X in `curl 'https://api.github.com/users/dmzoneill/repos?per_page=100' | jq 
   
   cd "$name"
   gh secret set profile_hook -r "dmzoneill/$name" -b "{{ secrets.PROFILE_HOOK }}"
+  git config --global user.email "dmz.oneill@gmail.com"
+  git config --global user.name "dmzoneill"
+  git remote set-url origin https://dmzoneill:${{ secrets.PROFILE_HOOK }}@github.com/dmzoneill/$name.git
   git add -A
   git commit -a -m "add github action and setup secret"
   git pull --rebase

c83a8bb95e54483731650e3811c566e27d464177
David O Neill <dmz.oneill@gmail.com>
2025-04-29 07:00:36 +0100

    Add new secrets to GitHub Actions workflow and update README with recent changes

--- setup.sh
+++ setup.sh
@@ -4,6 +4,21 @@ user=dmzoneill
 email=dmz.oneill@gmail.com
 pass=$PROFILE_HOOK
 
+# Secrets
+GITHUB_TOKEN="$GITHUB_TOKEN"
+PROFILE_HOOK="$PROFILE_HOOK"
+ACTIONS_RUNNER_DEBUG="$ACTIONS_RUNNER_DEBUG"
+ACTIONS_STEP_DEBUG="$ACTIONS_STEP_DEBUG"
+AI_API_KEY="$AI_API_KEY"
+AI_MODEL="$AI_MODEL"
+DOCKER_TOKEN="$DOCKER_TOKEN"
+PUBLISH="$PUBLISH"
+PYPI_TOKEN="$PYPI_TOKEN"
+WORDPRESS_APPLICATION="$WORDPRESS_APPLICATION"
+WORDPRESS_PASSWORD="$WORDPRESS_PASSWORD"
+WORDPRESS_URL="$WORDPRESS_URL"
+WORDPRESS_USERNAME="$WORDPRESS_USERNAME"
+
 page=1
 while true; do
   echo "Page: $page"
@@ -18,9 +33,26 @@ while true; do
     export GITHUB_TOKEN=$(cat .githubtoken)
     rm .githubtoken    
 
-    gh secret set profile_hook -r "$user/$name" -b "$pass"
+    # Skip the main repository
+    if [[ "$name" == "dmzoneill" ]]; then
+      echo "Skipping repository $name"
+      continue
+    fi
 
-    [[ "$name" == "dmzoneill" ]] && continue
+    # Set secrets for each repository except 'dmzoneill'
+    gh secret set GITHUB_TOKEN -r "$user/$name" -b "$GITHUB_TOKEN"
+    gh secret set PROFILE_HOOK -r "$user/$name" -b "$PROFILE_HOOK"
+    gh secret set ACTIONS_RUNNER_DEBUG -r "$user/$name" -b "$ACTIONS_RUNNER_DEBUG"
+    gh secret set ACTIONS_STEP_DEBUG -r "$user/$name" -b "$ACTIONS_STEP_DEBUG"
+    gh secret set AI_API_KEY -r "$user/$name" -b "$AI_API_KEY"
+    gh secret set AI_MODEL -r "$user/$name" -b "$AI_MODEL"
+    gh secret set DOCKER_TOKEN -r "$user/$name" -b "$DOCKER_TOKEN"
+    gh secret set PUBLISH -r "$user/$name" -b "$PUBLISH"
+    gh secret set PYPI_TOKEN -r "$user/$name" -b "$PYPI_TOKEN"
+    gh secret set WORDPRESS_APPLICATION -r "$user/$name" -b "$WORDPRESS_APPLICATION"
+    gh secret set WORDPRESS_PASSWORD -r "$user/$name" -b "$WORDPRESS_PASSWORD"
+    gh secret set WORDPRESS_URL -r "$user/$name" -b "$WORDPRESS_URL"
+    gh secret set WORDPRESS_USERNAME -r "$user/$name" -b "$WORDPRESS_USERNAME"
 
     action_file="https://github.com/$user/$name/blob/main/.github/workflows/main.yml?raw=true"
     exists=$(curl -L -s -o /tmp/last -w "%{http_code}" "$action_file")
@@ -44,7 +76,6 @@ while true; do
     
     (
       cd "$name" || exit 1
-      gh secret set profile_hook -r "$user/$name" -b "$pass"
       git config --global user.email $email
       git config --global user.name $user
 
